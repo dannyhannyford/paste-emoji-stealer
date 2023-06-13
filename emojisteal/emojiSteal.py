5c3b1fe1294acfd9fd4d9e2aa626505c50528aae
dannyhannyford <dannyhannyford@gmail.com>
2023-06-13 15:55:13 -0700

    emoji print2

--- emojisteal/emojiSteal.py
+++ emojisteal/emojiSteal.py
@@ -93,6 +93,7 @@ class EmojiSteal(commands.Cog):
                 try:
                     async with session.get(emoji.url) as resp:
                         image = io.BytesIO(await resp.read()).read()
+                    print(name, 'blank', emoji.name)
                     added = await ctx.guild.create_custom_emoji(name=name or emoji.name, image=image)
                 except Exception as error:
                     return await ctx.send(f"{EMOJI_FAIL} {emoji.name}, {type(error).__name__}: {error}")

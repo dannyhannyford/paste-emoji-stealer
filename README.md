e39bb702cf150dd95e6eb0173d01fc42cc4f7a9c
dannyhannyford <dannyhannyford@gmail.com>
2023-06-13 12:39:25 -0700

    discord emoji steal

---
+++ README.md
@@ -0,0 +1,7 @@
+### Commands
+
+get_emoji - gets the image link for custom emojis or emoji ID
+
+steal - steals
+
+steal upload - steals and uploads
---
+++ emojiSteal.py
@@ -0,0 +1,118 @@
+import discord
+import io
+import re
+import aiohttp
+import asyncio
+from dataclasses import dataclass
+from redbot.core import commands
+from typing import Optional, List
+from itertools import zip_longest
+
+MISSING_EMOJIS = "Can't find emojis or stickers in that message."
+MISSING_REFERENCE = "Reply to a message with this command to steal an emoji."
+MESSAGE_FAIL = "I couldn't grab that message, sorry."
+EMOJI_FAIL = "❌ Failed to upload"
+EMOJI_SLOTS = "⚠ This server doesn't have any more space for emojis!"
+INVALID_EMOJI = "Invalid emoji or emoji ID."
+
+@dataclass(init=True, order=True, frozen=True)
+class StolenEmoji:
+    animated: bool
+    name:str
+    id: int
+
+    @property
+    def url(self):
+        return f"https://cdn.discordapp.com/emojis/{self.id}.{'gif' if self.animated else 'png'}"
+    
+    def __hash__(self):
+        return hash(self.id)
+    
+    def __eq__(self, other):
+        return isinstance(other, StolenEmoji) and self.id == other.id
+
+class EmojiSteal(commands.Cog):
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+
+    ## cleanup
+    async def cog_unload(self) -> None:
+        self.bot.tree.remove_command(self.steal_context_menu.name, type=self.steal_context_menu.type)
+        self.bot.tree.remove_command(self.steal_upload_context_menu)
+
+    @staticmethod
+    def get_emojis(content: str) -> Optional[List[StolenEmoji]]:
+        results = re.findall(r"<(a?):(\w+):(\d{10,20})>", content)
+        return [StolenEmoji(*result) for result in results]
+    
+    @staticmethod
+    def available_emoji_slots(guild: discord.Guild, animated: bool):
+        current_emojis = len([em for em in guild.emojis if em.animated == animated])
+        return guild.emoji_limit - current_emojis
+    
+    async def steal_ctx(self, ctx: commands.Context) -> Optional[List[StolenEmoji]]:
+        reference = ctx.message.reference
+        if not reference:
+            await ctx.send(MISSING_REFERENCE)
+            return None
+        message = await ctx.channel.fetch_message(reference.message_id)
+        if not message:
+            await ctx.send(MESSAGE_FAIL)
+            return None
+        emojis = self.get_emojis(message.content)
+        if not emojis:
+            await ctx.send(MISSING_EMOJIS)
+            return None
+        return emojis
+    
+    @commands.group(name="steal", aliases=["emojisteal"], invoke_without_command=True)
+    async def steal_command(self, ctx: commands.Context):
+        """Steals the emojis of the message you reply to. Can also upload them with [p]steal upload."""
+        if not (emojis := await self.steal_ctx(ctx)):
+            return
+        response = '\n'.join([emoji.url for emoji in emojis])
+        await ctx.send(response)
+
+    @steal_command.command(name="upload")
+    @commands.guild_only()
+    @commands.has_permissions(manage_emojis=True)
+    @commands.bot_has_permissions(manage_emojis=True, add_reactions=True)
+    async def steal_upload_command(self, ctx: commands.Context, *names: str):
+        """Steals emojis you reply to and uploads them to this server."""
+        if not (emojis := await self.steal_ctx(ctx)):
+            return
+        
+        names = [''.join(re.findall(r"\w+", name)) for name in names]
+        names = [name if len(name) >= 2 else None for name in names]
+        emojis = list(dict.fromkeys(emojis))
+
+        async with aiohttp.ClientSession() as session:
+            for emoji, name in zip_longest(emojis, names):
+                if not self.available_emoji_slots(ctx.guild, emoji.animated):
+                    return await ctx.send(EMOJI_SLOTS)
+                if not emoji:
+                    break
+                try:
+                    async with session.get(emoji.url) as resp:
+                        image = io.BytesIO(await resp.read()).read()
+                    added = await ctx.guild.create_custom_emoji(name=name or emoji.name, image=image)
+                except Exception as error:
+                    return await ctx.send(f"{EMOJI_FAIL} {emoji.name}, {type(error).__name__}: {error}")
+                try:
+                    await ctx.message.add_reaction(added)
+                except:
+                    pass
+
+    @commands.command()
+    async def get_emoji(self, ctx: commands.Context, *, emoji: str):
+        """Get the image link for custom emojis or an emoji ID."""
+        emoji = emoji.strip()
+        if emoji.isnumeric():
+            emojis = [StolenEmoji(False, "e", int(emoji)), StolenEmoji(True, "e", int(emoji))]
+        elif not (emojis := self.get_emojis(emoji)):
+            await ctx.send(INVALID_EMOJI)
+            return
+        await ctx.send('\n'.join(emoji.url for emoji in emojis))
+
+
